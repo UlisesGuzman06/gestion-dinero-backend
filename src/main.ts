6bbc70b555e7ac27f7984fb5ab19fe7b7f7195e8
Ulises Guzman <guzmangimenezulises06@gmail.com>
2026-04-14 09:21:54 -0300

    Update CORS to allow Vercel origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8001',
+    origin: [
+      'http://localhost:8001',
+      'https://gestion-dinero-front.vercel.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
